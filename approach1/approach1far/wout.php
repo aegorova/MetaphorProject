a0013e338d2bfa4e97ce97e8ef32ce159839d573
ladlab <ladlab.experimenter@gmail.com>
2012-12-05 05:26:50 -0800

    updated

--- approach1/approach1far/wout.php
+++ approach1/approach1far/wout.php
@@ -12,38 +12,56 @@
 <body>
 <div id=ex1 style="outline:#000 thin solid; display:table-cell; height:200px; width:450px; text-align:center">
 <?php 
+clearstatcache();
+
 $results_dir = "./results/";
 $post = $_POST["results"];
+
 if ($_POST["assignmentId"] == ""){
 	$theFileName = (string)(time() % 100000000);
 	$theFile = fopen($results_dir . $theFileName . ".csv", "w");
 	$results = explode(";", $_POST["results"]);
   	$subjectId = $theFileName;
+  	if(!file_exists($results_dir . $theFileName . ".csv"))
+  	{
+  		$back_up = fopen($results_dir . "backup_" . $subjectId . ".txt", "w");
+  	}
+  	else{
+  		echo("File written!");
+  	}
 }
 else{
 	$theFileName = $_POST["assignmentId"];
-	$theFile = fopen ($results_dir . $theFileName . ".csv", "w");
+	$theFile = fopen ($theFileName, "w");
 	$results = explode(";", $_POST["results"]);
   	$subjectId = $theFileName;
+  	if(!file_exists($theFile$results_dir . $theFileName . ".csv")
+  	{
+  		$back_up = fopen($results_dir . "backup_" . $subjectId . ".txt", "w");
+  	}
+  	else{
+  		echo("File written!");
+  	}
 	echo($subjectId);
   	echo("<form method='POST' action='http://mturk.com/mturk/externalSubmit'><input type='hidden' name='assignmentId' value='" . $subjectId . "'><input id='submitButton' type='submit' name='Submit' value='Submit'></form>");
 }
 
 $theFields = array("gender", "age", "language", "yrs_spoken", "bilingual", "bifluent", "nationality", "politics", "nativeparents", "bornhere", "livein", "yrs_in_country", "ethnicity", "religion", "occupation", "econ_status", "education");
 
-$txtFile = fopen($result_dir . $fileName . ".txt", "w");
-
 foreach ($theFields as $field) {
 	//  echo($field . ", " . $_POST[$field]);
 	fwrite($theFile, $field . ", " . $_POST[$field] . "\n");
-	fwrite($txtFile, $post);
+	fwrite($back_up, $field . ", " . $_POST[$field] . "\n");
 }
+
+fwrite($back_up, $post);
 
 foreach ($results as $value){
 	fwrite($theFile, $value);
 	//  echo($value . "\n");
 }
 
+fclose($back_up);
 fclose($theFile);
 echo("Thank you for your participation!");
 ?>
